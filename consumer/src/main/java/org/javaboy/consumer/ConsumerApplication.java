0fc1b999ee2bb842c0f8620d37b6edb713253402
江南一点雨 <wangsong0210@gmail.com>
2020-01-09 21:54:36 +0800

    服务消费升级改造

--- consumer/src/main/java/org/javaboy/consumer/ConsumerApplication.java
+++ consumer/src/main/java/org/javaboy/consumer/ConsumerApplication.java
@@ -2,12 +2,26 @@ package org.javaboy.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplateOne() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- consumer/src/main/java/org/javaboy/consumer/UseHelloController.java
+++ consumer/src/main/java/org/javaboy/consumer/UseHelloController.java
@@ -1,10 +1,12 @@
 package org.javaboy.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -38,7 +40,11 @@ public class UseHelloController {
     }
 
     @Autowired
+    @Qualifier("restTemplateOne")
+    RestTemplate restTemplateOne;
+    @Autowired
     DiscoveryClient discoveryClient;
+
     @GetMapping("/hello2")
     public String hello2() {
         List<ServiceInstance> list = discoveryClient.getInstances("provider");
@@ -51,52 +57,16 @@ public class UseHelloController {
                 .append(":")
                 .append(port)
                 .append("/hello");
-        HttpURLConnection con = null;
-        try {
-            URL url = new URL(sb.toString());
-            con = (HttpURLConnection) url.openConnection();
-            if (con.getResponseCode() == 200) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String s = br.readLine();
-                br.close();
-                return s;
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "error";
+        String s = restTemplateOne.getForObject(sb.toString(), String.class);
+        return s;
     }
 
-    int count = 0;
+    @Autowired
+    @Qualifier("restTemplate")
+    RestTemplate restTemplate;
+
     @GetMapping("/hello3")
     public String hello3() {
-        List<ServiceInstance> list = discoveryClient.getInstances("provider");
-        ServiceInstance instance = list.get((count++) % list.size());
-        String host = instance.getHost();
-        int port = instance.getPort();
-        StringBuffer sb = new StringBuffer();
-        sb.append("http://")
-                .append(host)
-                .append(":")
-                .append(port)
-                .append("/hello");
-        HttpURLConnection con = null;
-        try {
-            URL url = new URL(sb.toString());
-            con = (HttpURLConnection) url.openConnection();
-            if (con.getResponseCode() == 200) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String s = br.readLine();
-                br.close();
-                return s;
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "error";
+        return restTemplate.getForObject("http://provider/hello", String.class);
     }
 }
