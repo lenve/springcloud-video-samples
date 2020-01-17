515988f34c50a18494d6c77b35ac5674734e7e9e
江南一点雨 <wangsong0210@gmail.com>
2020-01-17 11:37:35 +0800

    Consul 集群注册

--- consul-provider/src/main/java/org/javaboy/consulprovider/HelloController.java
+++ consul-provider/src/main/java/org/javaboy/consulprovider/HelloController.java
@@ -1,12 +1,16 @@
 package org.javaboy.consulprovider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    Integer port;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello>>" + port;
     }
 }
