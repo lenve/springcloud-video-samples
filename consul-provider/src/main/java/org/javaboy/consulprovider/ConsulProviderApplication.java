512528936fb1cee44fca59b1ba55d2051b02cbaf
江南一点雨 <wangsong0210@gmail.com>
2020-01-17 11:28:26 +0800

    Consul 单节点注册

---
+++ consul-provider/src/main/java/org/javaboy/consulprovider/ConsulProviderApplication.java
@@ -0,0 +1,15 @@
+package org.javaboy.consulprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulProviderApplication.class, args);
+    }
+
+}
---
+++ consul-provider/src/test/java/org/javaboy/consulprovider/ConsulProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.consulprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
