7146d2166cab0adbf60fc1c10d1acf14af7c9d2c
江南一点雨 <wangsong0210@gmail.com>
2020-02-19 00:26:38 +0800

    微服务中的断路器

--- resilience4j-2/src/main/java/org/javaboy/resilience4j2/HelloService.java
+++ resilience4j-2/src/main/java/org/javaboy/resilience4j2/HelloService.java
@@ -1,16 +1,22 @@
 package org.javaboy.resilience4j2;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@Retry(name = "retryA")//表示要使用的重试策略
+@CircuitBreaker(name = "cbA", fallbackMethod = "error")
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
+
     public String hello() {
         return restTemplate.getForObject("http://localhost:1113/hello", String.class);
+    }
+
+    public String error(Throwable t) {
+        return "error";
     }
 }
