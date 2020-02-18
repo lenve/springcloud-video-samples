00c89a81e8569e16b0e2e871fd24b5fefaabf7f5
江南一点雨 <wangsong0210@gmail.com>
2020-02-18 20:54:02 +0800

    Resilience4j-RateLimiter

--- resilience4j/src/test/java/org/javaboy/resilience4j/Resilience4jTest.java
+++ resilience4j/src/test/java/org/javaboy/resilience4j/Resilience4jTest.java
@@ -3,11 +3,18 @@ package org.javaboy.resilience4j;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import io.vavr.CheckedFunction0;
+import io.vavr.CheckedRunnable;
 import io.vavr.control.Try;
 import org.junit.Test;
 
+import javax.swing.*;
 import java.time.Duration;
+import java.util.Date;
 
 public class Resilience4jTest {
     @Test
@@ -55,5 +62,46 @@ public class Resilience4jTest {
                 .map(v -> v + " hello world");
         System.out.println(result.isSuccess());
         System.out.println(result.get());
+    }
+
+    @Test
+    public void test3() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitRefreshPeriod(Duration.ofMillis(1000))
+                .limitForPeriod(4)
+                .timeoutDuration(Duration.ofMillis(1000))
+                .build();
+        RateLimiter rateLimiter = RateLimiter.of("javaboy", config);
+        CheckedRunnable checkedRunnable = RateLimiter.decorateCheckedRunnable(rateLimiter, () -> {
+            System.out.println(new Date());
+        });
+        Try.run(checkedRunnable)
+                .andThenTry(checkedRunnable)
+                .andThenTry(checkedRunnable)
+                .andThenTry(checkedRunnable)
+                .onFailure(t -> System.out.println(t.getMessage()));
+    }
+
+    @Test
+    public void test4() {
+        RetryConfig config = RetryConfig.custom()
+                //重试次数
+                .maxAttempts(2)
+                //重试间隔
+                .waitDuration(Duration.ofMillis(500))
+                //重试异常
+                .retryExceptions(RuntimeException.class)
+                .build();
+        Retry retry = Retry.of("javaboy", config);
+        Retry.decorateRunnable(retry, new Runnable() {
+            int count = 0;
+            //开启了重试功能之后，run 方法执行时，如果抛出异常，会自动触发重试功能
+            @Override
+            public void run() {
+                if (count++ < 3) {
+                    throw new RuntimeException();
+                }
+            }
+        }).run();
     }
 }
