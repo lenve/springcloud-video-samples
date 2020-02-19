d3ec94431cbcdf720fd0270438a990f39964c7f6
江南一点雨 <wangsong0210@gmail.com>
2020-02-19 13:07:54 +0800

    监控简介

---
+++ micrometer/src/main/java/org/javaboy/micrometer/MicrometerApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerApplication.class, args);
+    }
+
+}
---
+++ micrometer/src/test/java/org/javaboy/micrometer/MicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
