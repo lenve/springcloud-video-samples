71a0d247fb3a68be8bb7540dbbe0b27d8a616144
江南一点雨 <wangsong0210@gmail.com>
2020-02-23 10:40:49 +0800

    配置文件动态刷新

--- config-client/src/main/java/org/javaboy/configclient/HelloController.java
+++ config-client/src/main/java/org/javaboy/configclient/HelloController.java
@@ -1,10 +1,12 @@
 package org.javaboy.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${javaboy}")
     String javaboy;
