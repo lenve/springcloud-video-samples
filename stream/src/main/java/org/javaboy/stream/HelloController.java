413f980e13374f1cf9e4c8d520a1be0c7ebd6f74
江南一点雨 <wangsong0210@gmail.com>
2020-02-24 23:43:44 +0800

    Spring Cloud Stream 定时任务

--- stream/src/main/java/org/javaboy/stream/HelloController.java
+++ stream/src/main/java/org/javaboy/stream/HelloController.java
@@ -1,17 +1,24 @@
 package org.javaboy.stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
+    public final static Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Autowired
     MyChannel myChannel;
+
     @GetMapping("/hello")
     public void hello() {
-        myChannel.output().send(MessageBuilder.withPayload("hello spring cloud stream!").build());
+        logger.info("send msg:" + new Date());
+        myChannel.output().send(MessageBuilder.withPayload("hello spring cloud stream!").setHeader("x-delay", 3000).build());
     }
 }
--- stream/src/main/java/org/javaboy/stream/MsgReceiver2.java
+++ stream/src/main/java/org/javaboy/stream/MsgReceiver2.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
+import java.util.Date;
+
 @EnableBinding(MyChannel.class)
 public class MsgReceiver2 {
     public final static Logger logger = LoggerFactory.getLogger(MsgReceiver2.class);
 
     @StreamListener(MyChannel.INPUT)
     public void receive(Object payload) {
-        logger.info("received2:" + payload);
+        logger.info("received2:" + payload+":"+new Date());
     }
 }
